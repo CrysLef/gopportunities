a4732d315b8b944bec56f3dc614901923e875c2c
Crystian Lefundes <crystian.lf@gmail.com>
2023-10-08 11:27:55 -0300

    feat:Add database config

---
+++ config/sqlite.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"os"
+
+	"github.com/CrysLef/gopportunities/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+
+	_, err := os.Stat(dbPath)
+
+	if os.IsNotExist(err) {
+		logger.Info("Database file nout found, creating...")
+
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("SQLite opening error: %v", err)
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("sqlite automigration error: %v", err)
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func IsDBExist(dbPath string) (*gorm.DB, error) {
+	_, err := os.Stat(dbPath)
+
+	if os.IsNotExist(err) {
+		logger.Info("Database file nout found, creating...")
+
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+	return db, nil
+}
