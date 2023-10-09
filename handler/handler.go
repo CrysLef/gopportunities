95248153850b89153028ce64ce99f7a346405aa1
Crystian Lefundes <crystian.lf@gmail.com>
2023-10-09 11:56:26 -0300

    feat:Add CreateOpeningHandler logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/CrysLef/gopportunities/config"
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *config.Logger
+)
+
+func Init() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
