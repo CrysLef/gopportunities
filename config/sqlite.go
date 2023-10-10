326b8e38d80bad635232727f7f203182de4ee13c
Crystian <crystian.lf@gmail.com>
2023-10-09 21:28:05 -0300

    fix:Refactor sqlite isDBExist function

--- config/sqlite.go
+++ config/sqlite.go
@@ -8,42 +8,6 @@ import (
 	"gorm.io/gorm"
 )
 
-func InitializeSQLite() (*gorm.DB, error) {
-	logger := GetLogger("sqlite")
-	dbPath := "./db/main.db"
-
-	_, err := os.Stat(dbPath)
-
-	if os.IsNotExist(err) {
-		logger.Info("Database file nout found, creating...")
-
-		err = os.MkdirAll("./db", os.ModePerm)
-		if err != nil {
-			return nil, err
-		}
-
-		file, err := os.Create(dbPath)
-		if err != nil {
-			return nil, err
-		}
-		file.Close()
-	}
-
-	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
-	if err != nil {
-		logger.Errorf("SQLite opening error: %v", err)
-		return nil, err
-	}
-
-	err = db.AutoMigrate(&schemas.Opening{})
-	if err != nil {
-		logger.Errorf("sqlite automigration error: %v", err)
-		return nil, err
-	}
-
-	return db, nil
-}
-
 func IsDBExist(dbPath string) (*gorm.DB, error) {
 	_, err := os.Stat(dbPath)
 
@@ -61,5 +25,26 @@ func IsDBExist(dbPath string) (*gorm.DB, error) {
 		}
 		file.Close()
 	}
+	return db, nil
+}
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+
+	IsDBExist(dbPath)
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("SQLite opening error: %v", err)
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("sqlite automigration error: %v", err)
+		return nil, err
+	}
+
 	return db, nil
 }
