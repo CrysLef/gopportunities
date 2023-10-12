ce97c00422ca8cd323ddca421ea382eb333b628a
Crystian <crystian.lf@gmail.com>
2023-10-12 09:01:58 -0300

    docs:Add swagger documentation to create opening handler

---
+++ internal/handler/CreateOpening.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/CrysLef/gopportunities/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+// @BasePath /api/v1
+// @Summary Create opening
+// @Description Creating a new job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param request body CreateOpeningRequest true "Request body"
+// @Success 200 {object} CreateOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /opening [post]
+func CreateOpeningHandler(ctx *gin.Context) {
+
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	err := request.Validate()
+
+	if err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		SendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	err = db.Create(&opening).Error
+
+	if err != nil {
+		logger.Errorf("error creating opening: %v", err.Error())
+		SendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+	SendSuccess(ctx, "create-opening", opening)
+}
