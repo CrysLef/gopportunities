e2da9acd6fda89faa532c20fb5dad224584b895c
Crystian <crystian.lf@gmail.com>
2023-10-16 16:25:54 -0300

    feat:Refactor Create opening handler

--- internal/handler/CreateOpening.go
+++ internal/handler/CreateOpening.go
@@ -13,40 +13,41 @@ import (
 // @Tags Openings
 // @Accept json
 // @Produce json
-// @Param request body CreateOpeningRequest true "Request body"
-// @Success 200 {object} CreateOpeningResponse
+// @Param request body HandlerOpeningRequest true "Request body"
+// @Success 200 {object} HandlerOpeningResponse
 // @Failure 400 {object} ErrorResponse
 // @Failure 500 {object} ErrorResponse
 // @Router /opening [post]
 func CreateOpeningHandler(ctx *gin.Context) {
-
-	request := CreateOpeningRequest{}
+	opening := schemas.Opening{}
+	request := HandlerOpeningRequest{}
+	errResponse := ErrorResponse{}
 
 	ctx.BindJSON(&request)
 
-	err := request.Validate()
-
-	if err != nil {
-		logger.Errorf("validation error: %v", err.Error())
-		SendError(ctx, http.StatusBadRequest, err.Error())
+	if err := request.ValidateCreate(); err != nil {
+		errResponse.Message = err.Error()
+		errResponse.ErrorCode = http.StatusBadRequest
+		logger.Errorf("validation error: %v", errResponse.Message)
+		sendError(ctx, errResponse)
 		return
 	}
 
-	opening := schemas.Opening{
+	opening = schemas.Opening{
 		Role:     request.Role,
 		Company:  request.Company,
 		Location: request.Location,
-		Remote:   *request.Remote,
+		Remote:   request.Remote,
 		Link:     request.Link,
 		Salary:   request.Salary,
 	}
 
-	err = db.Create(&opening).Error
-
-	if err != nil {
-		logger.Errorf("error creating opening: %v", err.Error())
-		SendError(ctx, http.StatusInternalServerError, err.Error())
+	if err := db.Create(&opening).Error; err != nil {
+		errResponse.Message = err.Error()
+		errResponse.ErrorCode = http.StatusInternalServerError
+		logger.Errorf("error creating opening: %v", errResponse.Message)
+		sendError(ctx, errResponse)
 		return
 	}
-	SendSuccess(ctx, "create-opening", opening)
+	sendSuccess(ctx, "create-opening", opening)
 }
